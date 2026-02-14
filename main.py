e10ce18f9c0e64045aa75774e6a95a19a11bb9e9
asantan6 <asantan6@charlotte.edu>
2026-02-13 22:59:00 -0500

    Checking account Implementation onto main

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 # TODO: IMPORT SUBCLASSES WHEN FINISHED
-#import SavingsAccountClass
-#import CheckingAccountClass
+
+
+from SavingsAccountClass import SavingsAccount
+from CheckingAccountClass import CheckingAccount
 
 def main():
     # TODO: CREATE INSTANTIATIONS OF CHECKING AND SAVINGS ACCOUNT (2 EACH) FOR TESTING
+
+    jeff_checking = CheckingAccount("Jeff",600,10)
+    john_checking = CheckingAccount("John", 20000, 1001)
+
+    jeff_checking.transfer(20)
+    john_checking.transfer(500)
+    jeff_checking.print_customer_info()
+    john_checking.print_customer_info()
     pass
 
 if "__main__" == __name__:
